df61da73def0074a2902bd27ffda5ff030005a50
John Lindquist <johnlindquist@gmail.com>
2019-08-06 13:13:29 -0600

    Tue Aug  6 13:13:29 MDT 2019

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
   const response = await fetch(`https://placekitten.com/320/240`)
   const blob = await response.blob()
 
-  const url = URL.createObjectURL(blob.slice(0, 4000))
+  const url = URL.createObjectURL(blob.slice(0, 5000))
 
   const image = new Image()
   image.src = url
